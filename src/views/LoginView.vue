b30649c535a2e932dda0fc8868174b55793143fb
Ivan Mendoza <navi96@hotmail.es>
2022-10-07 22:34:47 -0500

    Added style to login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -18,4 +18,26 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.login {
+    margin-top: 40px;
+}
+input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+}
+button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+}
+p {
+    margin-top: 40px;
+    font-size: 13px;
+}
+p a {
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
